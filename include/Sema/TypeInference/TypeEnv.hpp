13d10801fd29d81dbacca03b06687254b54d209c
James Fang <james.fang.713@gmail.com>
2025-10-06 20:07:18 -0500

    TypeInference refactor

--- include/Sema/TypeInference/TypeEnv.hpp
+++ include/Sema/TypeInference/TypeEnv.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <optional>
-#include <ranges>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
@@ -14,57 +13,15 @@ namespace phi {
 class ValueDecl;
 class Type;
 
-//------------------------------------------------------------------------------
-// TypeEnv: methods on a class (OOP), not a raw struct
-//------------------------------------------------------------------------------
-
 class TypeEnv {
 public:
-  // Bind by declaration pointer (preferred: your DeclRefExpr should carry a
-  // decl*)
-  void bind(const ValueDecl *D, Polytype P) {
-    DeclMap.emplace(D, std::move(P));
-  }
+  void bind(const ValueDecl *D, Polytype P);
+  void bind(std::string Name, Polytype P);
+  void applySubstitution(const Substitution &S);
 
-  // Rare fallback by name (if a DeclRefExpr hasn't been resolved)
-  void bind(std::string Name, Polytype P) {
-    NameMap.emplace(std::move(Name), std::move(P));
-  }
-
-  std::optional<Polytype> lookup(const ValueDecl *Decl) const {
-    const auto It = DeclMap.find(Decl);
-    if (It != DeclMap.end())
-      return It->second;
-    return std::nullopt;
-  }
-
-  [[nodiscard]] std::optional<Polytype> lookup(const std::string &Name) const {
-    if (const auto It = NameMap.find(Name); It != NameMap.end())
-      return It->second;
-    return std::nullopt;
-  }
-
-  // Apply substitution to the whole env (used after unify steps)
-  void applySubstitution(const Substitution &S) {
-    for (auto &Decl : DeclMap | std::views::values)
-      Decl = S.apply(Decl);
-    for (auto &Name : NameMap | std::views::values)
-      Name = S.apply(Name);
-  }
-
-  // Free vars in env (for generalization)
-  [[nodiscard]] std::unordered_set<TypeVar> freeTypeVars() const {
-    std::unordered_set<TypeVar> Acc;
-    for (const auto &Decl : DeclMap | std::views::values) {
-      auto F = Decl.freeTypeVars();
-      Acc.insert(F.begin(), F.end());
-    }
-    for (const auto &Name : NameMap | std::views::values) {
-      auto F = Name.freeTypeVars();
-      Acc.insert(F.begin(), F.end());
-    }
-    return Acc;
-  }
+  [[nodiscard]] std::optional<Polytype> lookup(const ValueDecl *Decl) const;
+  [[nodiscard]] std::optional<Polytype> lookup(const std::string &Name) const;
+  [[nodiscard]] std::unordered_set<TypeVar> freeTypeVars() const;
 
 private:
   std::unordered_map<const ValueDecl *, Polytype> DeclMap;
---
+++ src/Sema/TypeInference/Inferencer/Unify.cpp
@@ -0,0 +1,195 @@
+#include "Diagnostics/DiagnosticBuilder.hpp"
+#include "Sema/TypeInference/Infer.hpp"
+#include "Sema/TypeInference/Substitution.hpp"
+#include "Sema/TypeInference/Types/Monotype.hpp"
+
+namespace phi {
+
+void TypeInferencer::unifyInto(Substitution &S, const Monotype &A,
+                               const Monotype &B) {
+  Substitution U = unify(S.apply(A), S.apply(B));
+  S.compose(U);
+}
+
+Substitution TypeInferencer::unify(const Monotype &A, const Monotype &B) {
+  // When both are variables, prefer to bind unconstrained to constrained
+  if (A.isVar() && B.isVar()) {
+    const auto &VarA = A.asVar();
+    const auto &VarB = B.asVar();
+
+    // If A is constrained and B is not, bind B to A
+    if (VarA.Constraints && !VarB.Constraints) {
+      return unifyVar(B, A);
+    }
+    // If B is constrained and A is not, bind A to B
+    if (VarB.Constraints && !VarA.Constraints) {
+      return unifyVar(A, B);
+    }
+    // If both have constraints, check compatibility in bindWith
+    // Default to binding A to B
+    return unifyVar(A, B);
+  }
+
+  if (B.isVar()) {
+    return unifyVar(B, A);
+  }
+
+  struct UnifyVisitor {
+    TypeInferencer &TI;
+    const Monotype &A;
+    const Monotype &B;
+
+    // TypeVar case
+    Substitution operator()(const TypeVar &_) const {
+      return TI.unifyVar(A, B);
+    }
+
+    // TypeCon case
+    Substitution operator()(const TypeCon &_) const {
+      return TI.unifyCon(A, B);
+    }
+
+    // TypeApp case
+    Substitution operator()(const TypeApp &_) const {
+      return TI.unifyApp(A, B);
+    }
+
+    // TypeFun case
+    Substitution operator()(const TypeFun &_) const {
+      return TI.unifyFun(A, B);
+    }
+  };
+  return std::visit(UnifyVisitor{.TI = *this, .A = A, .B = B}, A.getPtr());
+}
+
+Substitution TypeInferencer::unifyVar(const Monotype &Var, const Monotype &B) {
+  std::println("Unifying var {} with {}", Var.toString(), B.toString());
+  if (Var.isVar()) {
+    auto varConstraints = Var.asVar().Constraints;
+    std::println("  Var {} has {} constraints", Var.asVar().Id,
+                 varConstraints ? varConstraints->size() : 0);
+  }
+  if (B.isVar()) {
+    auto bConstraints = B.asVar().Constraints;
+    std::println("  B {} has {} constraints", B.asVar().Id,
+                 bConstraints ? bConstraints->size() : 0);
+  }
+
+  auto Res = Var.asVar().bindWith(B);
+  if (Res) {
+    std::println("  Unification succeeded");
+    return *Res;
+  }
+
+  std::println("  Unification failed");
+  DiagMan->emit(Res.error());
+  return Substitution{};
+}
+
+Substitution TypeInferencer::unifyCon(const Monotype &A, const Monotype &B) {
+  if (!B.isCon()) {
+    emitUnifyError(
+        A, B,
+        std::format("cannot unify `{}` with `{}`", A.toString(), B.toString()));
+    return Substitution{};
+  }
+
+  if (A.asCon() != B.asCon()) {
+    emitUnifyError(
+        A, B,
+        std::format("cannot unify `{}` with `{}`", A.toString(), B.toString()));
+    return Substitution{};
+  }
+
+  Substitution S;
+  for (size_t i = 0; i < B.asCon().Args.size(); ++i) {
+    auto ArgA = A.asCon().Args[i];
+    auto ArgB = B.asCon().Args[i];
+    auto Sub = unify(S.apply(ArgA), S.apply(ArgB));
+    S.compose(Sub);
+  }
+  return S;
+}
+
+Substitution TypeInferencer::unifyApp(const Monotype &A, const Monotype &B) {
+  if (!B.isApp()) {
+    emitUnifyError(
+        A, B,
+        std::format("cannot unify `{}` with `{}`", A.toString(), B.toString()));
+    return Substitution{};
+  }
+
+  if (A.asApp() != B.asApp()) {
+    emitUnifyError(
+        A, B,
+        std::format("cannot unify `{}` with `{}`", A.toString(), B.toString()));
+    return Substitution{};
+  }
+
+  Substitution S;
+  for (size_t i = 0; i < B.asApp().Args.size(); ++i) {
+    auto ArgA = A.asApp().Args[i];
+    auto ArgB = B.asApp().Args[i];
+    auto Sub = unify(S.apply(ArgA), S.apply(ArgB));
+    S.compose(Sub);
+  }
+  return S;
+}
+
+Substitution TypeInferencer::unifyFun(const Monotype &A, const Monotype &B) {
+  if (!B.isFun()) {
+    emitUnifyError(
+        A, B,
+        std::format("cannot unify `{}` with `{}`", A.toString(), B.toString()));
+    return Substitution{};
+  }
+
+  if (A.asFun() != B.asFun()) {
+    emitUnifyError(
+        A, B,
+        std::format("function arity mismatch: {} vs {}",
+                    A.asFun().Params.size(), B.asFun().Params.size()),
+        std::optional<std::string>("check the number of parameters"));
+    return Substitution{};
+  }
+
+  Substitution S;
+  for (size_t i = 0; i < A.asFun().Params.size(); ++i) {
+    auto ParamA = A.asFun().Params[i];
+    auto ParamB = B.asFun().Params[i];
+    auto ParamSubst = unify(S.apply(ParamA), S.apply(ParamB));
+    S.compose(ParamSubst);
+  }
+
+  auto RetSubst = unify(S.apply(*A.asFun().Ret), S.apply(*B.asFun().Ret));
+  S.compose(RetSubst);
+  return S;
+}
+
+void TypeInferencer::emitUnifyError(const Monotype &A, const Monotype &B,
+                                    const std::string &TopMsg,
+                                    const std::optional<std::string> &Note) {
+  auto Builder = error(TopMsg);
+
+  if (A.getLocation().Line >= 0) {
+    Builder = Builder.with_primary_label(
+        SrcSpan(A.getLocation()),
+        std::format("`{}` inferred/used here", A.toString()));
+    if (B.getLocation().Line >= 0) {
+      Builder = Builder.with_secondary_label(
+          SrcSpan(B.getLocation()),
+          std::format("conflicts with `{}` here", B.toString()));
+    }
+  } else if (B.getLocation().Line >= 0) {
+    Builder = Builder.with_primary_label(
+        SrcSpan(B.getLocation()),
+        std::format("`{}` inferred/used here", B.toString()));
+  }
+
+  if (Note)
+    Builder = Builder.with_note(*Note);
+
+  Builder.emit(*DiagMan);
+}
+
+} // namespace phi
