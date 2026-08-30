9fdb84b81e49b936c51f3b9e7774f42aa129e860
James Fang <james.fang.713@gmail.com>
2026-08-30 01:30:18 -0500

    Feat: emit warnings for unused variables

--- src/mir/checks.rs
+++ src/mir/checks.rs
@@ -3,7 +3,7 @@ pub mod lattice;
 mod always_return;
 pub(crate) mod borrowck;
 pub(crate) mod constck;
-mod never_read;
+pub(crate) mod never_read;
 
 use crate::mir::lower::Mir;
 
@@ -11,4 +11,5 @@ pub fn run_checks(mir: &Mir) {
     constck::check(mir);
     always_return::check(mir);
     borrowck::check(mir);
+    never_read::check(mir);
 }
