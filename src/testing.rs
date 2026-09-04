cc638c710d396f8d45c8fa7cdee92f624484f19b
James Fang <james.fang.713@gmail.com>
2026-09-04 15:09:32 -0500

    test: add a shared operator-trait preamble for fixtures that need it

--- src/testing.rs
+++ src/testing.rs
@@ -63,6 +63,32 @@ pub fn checker_through(hir: &Hir, stage: Stage) -> Typeck<'_> {
     checker
 }
 
+pub const OPS_PREAMBLE: &str = "module core::ops;
+     public trait Add { fun add(&self, other: &Self) -> Self; }
+     public trait Sub { fun sub(&self, other: &Self) -> Self; }
+     public trait Mul { fun mul(&self, other: &Self) -> Self; }
+     public trait Div { fun div(&self, other: &Self) -> Self; }
+     public trait Rem { fun rem(&self, other: &Self) -> Self; }
+     public trait Eq { fun eq(&self, other: &Self) -> bool; }
+     public trait Comparable { fun less_than(&self, other: &Self) -> bool; }
+     public trait Not { fun not(&self) -> Self; }
+     extend bool with Not { fun not(&self) -> Self { return !*self; } }
+     extend i32 with Add { fun add(&self, other: &Self) -> Self { return *self + *other; } }
+     extend i32 with Sub { fun sub(&self, other: &Self) -> Self { return *self - *other; } }
+     extend i32 with Mul { fun mul(&self, other: &Self) -> Self { return *self * *other; } }
+     extend i32 with Div { fun div(&self, other: &Self) -> Self { return *self / *other; } }
+     extend i32 with Rem { fun rem(&self, other: &Self) -> Self { return *self % *other; } }
+     extend i32 with Eq { fun eq(&self, other: &Self) -> bool { return *self == *other; } }
+     extend i32 with Comparable { fun less_than(&self, other: &Self) -> bool { return *self < *other; } }
+     extend f64 with Add { fun add(&self, other: &Self) -> Self { return *self + *other; } }
+     extend f64 with Sub { fun sub(&self, other: &Self) -> Self { return *self - *other; } }
+     extend f64 with Mul { fun mul(&self, other: &Self) -> Self { return *self * *other; } }
+     extend f64 with Div { fun div(&self, other: &Self) -> Self { return *self / *other; } }
+     extend f64 with Rem { fun rem(&self, other: &Self) -> Self { return *self % *other; } }
+     extend f64 with Eq { fun eq(&self, other: &Self) -> bool { return *self == *other; } }
+     extend f64 with Comparable { fun less_than(&self, other: &Self) -> bool { return *self < *other; } }
+     ";
+
 // TODO: move this into DiagCtx
 pub fn messages() -> Vec<String> {
     DiagCtx::diagnostics()
